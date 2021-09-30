0ed171cf7ec7b78a528133f6b5be976ce70b44b0
Scotty Yackzan <syackzan@gmail.com>
2021-09-29 18:09:44 -0700

    Added Relationships inside models index.js + started Creating json files for seeding

--- models/index.js
+++ models/index.js
@@ -2,18 +2,32 @@ const User = require('./User');
 const Blog = require('./Blog');
 const Comment = require('./Comment');
 
+User.hasMany(Blog, {
+    foreignKey: "user_id",
+    onDelete: "CASCADE",
+})
 
+Blog.belongsTo(User, {
+    foreignKey: 'user_id'
+})
 
-// GeneralContractors.hasMany(Project, {
-//     foreignKey: 'general_contractor_id',
-//     onDelete: 'CASCADE'
-// });
+Blog.hasMany(Comment, {
+    foreignKey: "blog_id",
+    onDelete: "CASCADE"
+})
 
-// Project.belongsTo(GeneralContractors, {
-//     foreignKey: 'general_contractor_id'
-// });
+Comment.belongsTo(Blog, {
+    foreignKey: 'blog_id'
+})
 
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: "Cascade"
+})
 
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+})
 
 
 module.exports = { 
