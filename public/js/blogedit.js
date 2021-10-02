7b9969ee14c91201472ca00f06bfcd1ae29fda5f
Scotty Yackzan <syackzan@gmail.com>
2021-10-02 13:02:04 -0700

    Editing Blog won't reload the page, it automatically refreshes and doesn't have time to change the document location

--- public/js/blogedit.js
+++ public/js/blogedit.js
@@ -1,32 +1,38 @@
 //Elements for Editing a Post//
 const editBtn = document.getElementById("editBtn");
+let userId = document.getElementById("userId").textContent;
+console.log(userId);
 
 //Elements for Deleting a Post//
 const deleteBtn = document.getElementById('deleteBtn');
 
 //Trigger Post Route to update Blog Post//
-const editPost = async () => {
+const editPost = async (event) => {
+    event.preventDefault();
+    console.log("hello");
 
     let title = document.getElementById('editTitle').value.trim();
     let contents = document.getElementById('editContent').value.trim();
     let id = document.getElementById("EoDid").textContent;
-    let userId = document.getElementById("userId").textContent;
     
-    const response = await fetch(`/api/blog/u/${id}`, {
+    
+    let response = await fetch(`/api/blog/u/${id}`, {
         method: "PUT",
         body: JSON.stringify({ title, contents }),
         headers: { 'Content-Type': 'application/json' }
     })
-        
+       
+    
     if (response.ok) {
-        document.location.replace(`/dashboard/${userId}`);
+        //document.location.replace(`/dashboard/${userId}`)
       } else {
         alert(response.statusText);
       }
 }
 
 //Triggers Route to delete Element
-const deletePost = async () => {
+const deletePost = async (event) => {
+    event.preventDefault();
 
     let answer = confirm("Are you sure you want to delete");
 
@@ -35,17 +41,27 @@ const deletePost = async () => {
         let id = document.getElementById("EoDid").textContent;
         let userId = document.getElementById("userId").textContent;
         
-        const response = await fetch(`/api/blog/${id}`, {
+        let response = await fetch(`/api/blog/${id}`, {
             method: "DELETE",
         })
             
-        location.href = (`/dashboard/${userId}`);
+        
+        if (response.ok) {
+            document.location.replace(`/dashboard/${userId}`)
+          } else {
+            alert(response.statusText);
+          }
+
+    } else {
+        return;
     }
-    return;
 }
 
 //Edit Post Event Listener//
-editBtn.addEventListener('click', editPost)
+editBtn.addEventListener('submit', function(event){
+    event.preventDefault()
+    editPost()
+})
 
 //Delete Event Listener//
-deleteBtn.addEventListener('click', deletePost)
+deleteBtn.addEventListener('submit', deletePost)
